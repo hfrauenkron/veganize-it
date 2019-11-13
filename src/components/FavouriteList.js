83598c5e1af22c27d48051ef63f076a6cd3b5477
Hanna Frauenkron <h.frauenkron@mail.de>
2019-11-13 06:25:49 +0100

    adjust toggle Favourites

--- src/components/FavouriteList.js
+++ src/components/FavouriteList.js
@@ -3,10 +3,14 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import BadgeButtonClose from "./BadgeButtonClose";
 import { Link } from "react-router-dom";
-import { getFavouritesFromStorage } from "../api/storage";
+import {
+  getFavouritesFromStorage,
+  setFavouritesToStorage
+} from "../api/storage";
 
 const FavouriteListDiv = styled.div`
   max-width: 500px;
+  margin-top: 10px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -17,13 +21,35 @@ const StyledLink = styled(Link)`
   color: #fff;
 `;
 
-export default function FavouriteList({ onClick }) {
+export default function FavouriteList() {
   const [favourites, setFavourites] = useState(getFavouritesFromStorage());
+
+  function handleRemoveFavourite(favourite) {
+    const existingIndex = favourites.findIndex(
+      existingFavourite => existingFavourite.id === favourite.id
+    );
+    if (existingIndex !== -1) {
+      const favouritesList = [...favourites];
+      favouritesList.splice(existingIndex, 1);
+      setFavourites(favouritesList);
+    }
+  }
+
+  React.useEffect(() => {
+    setFavouritesToStorage(favourites);
+  }, [favourites]);
+
+  console.log(favourites);
 
   return (
     <FavouriteListDiv>
       {favourites.map(favourite => (
-        <BadgeButtonClose key={favourite.id} onClick={onClick}>
+        <BadgeButtonClose
+          key={favourite.id}
+          onClick={() => {
+            handleRemoveFavourite(favourite);
+          }}
+        >
           <StyledLink to={`${favourite.url}#${favourite.name.toLowerCase()}`}>
             {favourite.name.toUpperCase()}
           </StyledLink>
@@ -32,3 +58,7 @@ export default function FavouriteList({ onClick }) {
     </FavouriteListDiv>
   );
 }
+
+FavouriteList.propTypes = {
+  onClick: PropTypes.func
+};
